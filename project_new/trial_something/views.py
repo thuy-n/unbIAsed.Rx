d5ff98296827cf3417b972f53b05a679094666f0
anagordon <anbgordon@gmail.com>
2024-07-03 18:25:28 -0400

    image container update

--- project_new/trial_something/views.py
+++ project_new/trial_something/views.py
@@ -332,15 +332,3 @@ def unsave_drug():
     drug.is_saved = False
     db.session.commit()
     return redirect(url_for('views.home'))
-
-# @views.route('/delete-note', methods=['POST'])
-# def delete_note():  
-#     note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
-#     noteId = note['noteId']
-#     note = Note.query.get(noteId)
-#     if note:
-#         if note.user_id == current_user.id:
-#             db.session.delete(note)
-#             db.session.commit()
-
-#     return jsonify({})
