92c77f4c86014daf2440e0e5a6742a9871f08606
anagordon <anbgordon@gmail.com>
2024-07-05 13:15:58 -0400

    Update auth.py

--- project_new/trial_something/auth.py
+++ project_new/trial_something/auth.py
@@ -182,7 +182,8 @@ def sign_up():
         last_name = request.form.get('lastName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-        sex = request.form.get('sexSelect')
+        sexs = request.form.get('sexSelect')
+        sex = sexs.value 
 
         user = User.query.filter_by(email=email).first()
         if user:
