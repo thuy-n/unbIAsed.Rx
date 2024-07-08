a709a4671d2938d824eebea55d596a5e0d70e624
anagordon <anbgordon@gmail.com>
2024-07-07 21:10:16 -0400

    Update auth.py

--- project_new/trial_something/auth.py
+++ project_new/trial_something/auth.py
@@ -210,6 +210,7 @@ def sign_up():
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
+            session['device_seen'] = True
             flash('Account created!', category='success')
             return redirect(url_for('views.home'))
             
