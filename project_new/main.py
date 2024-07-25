1cfa0ab921341a18dc4f55adfe4ecd481c15885d
anagordon <anbgordon@gmail.com>
2024-07-25 14:17:00 -0400

    Update main.py

--- project_new/main.py
+++ project_new/main.py
@@ -1,21 +1,12 @@
 from trial_something import create_app
-from OpenSSL import SSL 
 
 app = create_app()
 
 if __name__ == '__main__':
     def divide_by_100(x):
         return x/100
-    
-    # Define SSL certificate and key file paths 
-    CERT_FILE = "/path/to/cert.pem" 
-    KEY_FILE = "/path/to/key.pem" 
 
-    # Create SSL context 
-    context = SSL.Context(SSL.PROTOCOL_TLSv1_2) 
-    context.load_cert_chain(CERT_FILE, KEY_FILE) 
-
-    app.run(debug=True, host='165.227.32.65', port=80, ssl_context=context) 
+    app.run(debug=True, host='165.227.32.65', port=80) 
 
 
 
