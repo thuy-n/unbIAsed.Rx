a9cac73a8d208dcb1ae255df0f0b500e8d100ea2
anagordon <anbgordon@gmail.com>
2024-08-24 19:25:34 -0400

    edits

--- project_new/trial_something/views.py
+++ project_new/trial_something/views.py
@@ -303,9 +303,9 @@ def home():
                 male_ratio=row['Male proportion in studies'],
                 prevalence=relative_image_path,
                 path_prevalence = f"static/images/prevalence/{row['Indication'].lower()}.png",
-                prevFemale = round(row['Prevalence Women'],2),
-                prevMale = round(row['Prevalence Men'],2),
-                prevBoth = round(row['Prevalence Both Genders'],2)
+                prevFemale = round(row['Prevalence Women'],3),
+                prevMale = round(row['Prevalence Men'],3),
+                prevBoth = round(row['Prevalence Both Genders'],3)
             )
 
             db.session.add(drug)
