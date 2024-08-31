a1b395af0b138aa306f31fd14ea776f0ce2e3780
anagordon <anbgordon@gmail.com>
2024-08-31 16:27:18 -0400

    Update auth.py

--- project_new/trial_something/auth.py
+++ project_new/trial_something/auth.py
@@ -245,27 +245,27 @@ def delete_account():
 
 @auth.route('/search', methods=['GET', 'POST'])
 def search():
-    # Retrieve the search term from the form or query parameters
     if request.method == 'POST':
         search_term = request.form.get('query')
     else:
         search_term = request.args.get('query')
 
-    # Check if the search term is empty
-    if not search_term:
+    if search_term == "":
         flash("Please enter a search term.", category='error')
         return redirect(url_for('views.home'))
 
     # Perform a case-insensitive pattern search
+    search_term = str(search_term) if search_term is not None else ''
     search_term = '%' + search_term + '%'
+    # results = Drugs.query.filter(Drugs.name.ilike(search_term)).all()
     results = Drugs.query.filter(or_(Drugs.name.ilike(search_term), Drugs.disease.ilike(search_term))).all()
 
-    # If no exact match is found, find the closest match
     if not results:
+    # If no exact match is found, find the closest match
         all_drugs = Drugs.query.all()
         all_drug_names = [drug.name for drug in all_drugs]
-        closest_match_result = process.extractOne(search_term.strip('%'), all_drug_names)
-        if closest_match_result:
+        closest_match_result = process.extractOne(search_term, all_drug_names)
+        if closest_match_result:  # Check if closest_match_result is not None
             closest_match_name = closest_match_result[0]
             closest_match = Drugs.query.filter_by(name=closest_match_name).first()
             if closest_match:
@@ -273,90 +273,11 @@ def search():
         else:
             flash("No drug found with that name.", category='error')
             return redirect(url_for('views.home'))
-
-    # Initialize variables
-    disease_prevalence = None
-    prediction_risk = ""
-    flash_message_risk = ""
-    errorFlash = False
-    result_string_pred = ""
-
-    # Retrieve form inputs
-    drug_search = request.form.get('drugName')
-    disease_search = request.form.get('drugCondition')
-    drug_id = request.form.get('drug_id')
-
-    # Check for missing inputs
-    if not drug_id or not drug_search or not disease_search:
-        flash("Please fill in all fields.", category='error')
-        return redirect(url_for('views.home'))
-
-    # Convert inputs to uppercase
-    drug_search = drug_search.upper()
-    disease_search = disease_search.upper()
-
-    # Calculate prediction risk
-    prediction_risk = get_model(drug_search, disease_search)
-
-    # Convert drug_id to integer
-    if prediction_risk is not None:
-        drug_id = int(drug_id)
-
-    # Initialize risk values
-    F = 0
-    M = 0
-
-    # Calculate risk values based on prediction
-    if prediction_risk is not None:
-        M = 100 - prediction_risk
-        F = prediction_risk
-
-        if current_user.is_authenticated and current_user.sexe is not None:
-            if current_user.sexe.lower() == 'male':
-                if M > F:
-                    R = M - F
-                    result_string_pred = (
-                        f"The predicted risk for male patients of developing an adverse drug reaction to {drug_search} given the condition {disease_search} is <b>{round(M, 2)}%</b>. <br><br>"
-                        f"Male patients have a <b>{round(R, 2)}%</b> lower risk of developing a reaction compared to female patients. <br>"
-                    )
-                else:
-                    R = F - M
-                    result_string_pred = (
-                        f"The predicted risk for male patients of developing an adverse drug reaction to {drug_search} given the condition {disease_search} is <b>{round(M, 2)}%</b>. <br><br>"
-                        f"Male patients have an additional <b>{round(R, 2)}%</b> risk of developing a reaction compared to female patients.<br>"
-                    )
-            elif current_user.sexe.lower() == 'female':
-                if F > M:
-                    R = F - M
-                    result_string_pred = (
-                        f"The predicted risk for female patients of developing an adverse drug reaction to {drug_search} given the condition {disease_search} is <b>{round(F, 2)}%</b>. <br><br>"
-                        f"Female patients have a <b>{round(R, 2)}%</b> lower risk of developing a reaction compared to male patients. <br>"
-                    )
-                else:
-                    R = M - F
-                    result_string_pred = (
-                        f"The predicted risk for female patients of developing an adverse drug reaction to {drug_search} given the condition {disease_search} is <b>{round(F, 2)}%</b>. <br><br>"
-                        f"Female patients have an additional <b>{round(R, 2)}%</b> risk of developing a reaction compared to male patients. <br>"
-                    )
-        else:
-            if F > M:
-                R = F - M
-                result_string_pred = (
-                    f"The predicted risk for female patients of developing an adverse drug reaction to {drug_search} given the condition {disease_search} is <b>{round(F, 2)}%</b>. <br><br>"
-                    f"Female patients have a <b>{round(R, 2)}%</b> lower risk of developing a reaction compared to male patients.<br>"
-                )
-            else:
-                R = M - F
-                result_string_pred = (
-                    f"The predicted risk for female patients of developing an adverse drug reaction to {drug_search} given the condition {disease_search} is <b>{round(F, 2)}%</b>. <br><br>"
-                    f"Female patients have an additional <b>{round(R, 2)}%</b> risk of developing a reaction compared to male patients.<br>"
-                )
-
-    # Render the appropriate template with the results
+    
     user_agent = request.headers.get('User-Agent').lower()
     if 'mobile' in user_agent:
-        return render_template("search_results-mobile.html", results=results, user=current_user, disease_prevalence=disease_prevalence, result_string_pred=result_string_pred, result_drug_id=drug_id)
-    return render_template("search_results.html", results=results, user=current_user, disease_prevalence=disease_prevalence, result_string_pred=result_string_pred, result_drug_id=drug_id)
+        return render_template("search_results-mobile.html", results=results, user=current_user)
+    return render_template("search_results.html", results=results, user=current_user)
 
 def preprocess(sentence):
     # Remove punctuation
