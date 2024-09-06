f4220995240d54d2bb3505648f483cf2123055a8
anagordon <anbgordon@gmail.com>
2024-09-06 14:45:36 -0400

    Update views.py

--- project_new/trial_something/views.py
+++ project_new/trial_something/views.py
@@ -356,12 +356,12 @@ def calc_risk():
     result_string_pred = result_string_pred  
        
     if prediction_risk is not None and current_user.is_authenticated:
-        # if from_saved_page == 'True':
-        drugs = current_user.drugs
-        user_agent = request.headers.get('User-Agent').lower()
-        if 'mobile' in user_agent:
-            return render_template("saved-mobile.html", drugs=drugs, user=current_user, disease_prevalence=disease_prevalence, result_string_pred=result_string_pred, result_drug_id=drug_id)
-        return render_template("saved.html", drugs=drugs, user=current_user, disease_prevalence=disease_prevalence, result_string_pred=result_string_pred, result_drug_id=drug_id)
+        if from_saved_page == 'true':
+            drugs = current_user.drugs
+            user_agent = request.headers.get('User-Agent').lower()
+            if 'mobile' in user_agent:
+                return render_template("saved-mobile.html", drugs=drugs, user=current_user, disease_prevalence=disease_prevalence, result_string_pred=result_string_pred, result_drug_id=drug_id)
+            return render_template("saved.html", drugs=drugs, user=current_user, disease_prevalence=disease_prevalence, result_string_pred=result_string_pred, result_drug_id=drug_id)
 
     user_agent = request.headers.get('User-Agent').lower()
     if 'mobile' in user_agent:
